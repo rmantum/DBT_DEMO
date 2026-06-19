0d00ff59f42aa95d8ca82ae2a911197c82b5692a
MD Mahbub Alam <mahbub.alam@novon.com.au>
2026-06-19 08:22:44 +0000

    date_utls macro

--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -1,15 +1,15 @@
 WITH CTE AS (
 Select
 
-to_timestamp(started_at) as STARTED_AT,
-date(to_timestamp(started_at)) as DATE_STARTED_AT,
-HOUR(to_timestamp(started_at)) AS HOUR_STARTED_AT,
-DAYNAME(to_timestamp(started_at)) AS DAYNAME,
-CASE 
-    WHEN DAYNAME(to_timestamp(started_at)) IN ('Sat', 'Sun')
-    THEN 'WEEKEND'
-    ELSE 'BUSINESSDAY'
-END AS DAY_TYPE
+TO_TIMESTAMP(STARTED_AT) as STARTED_AT,
+DATE(to_timestamp(STARTED_AT)) as DATE_STARTED_AT,
+HOUR(to_timestamp(STARTED_AT)) AS HOUR_STARTED_AT,
+DAYNAME(to_timestamp(STARTED_AT)) AS DAYNAME,
+
+{{ day_type('STARTED_AT') }} AS DAY_TYPE, 
+
+{{ get_season('STARTED_AT') }} AS STATION_OF_YEAR
+
 from
 {{ source('demo', 'bike') }}
 where STARTED_AT != 'started_at'
