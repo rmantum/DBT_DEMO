87135a1b441828e8c671039ea8b6d2073b436945
MD Mahbub Alam <mahbub.alam@novon.com.au>
2026-06-22 04:53:33 +0000

    sdjfhvsdakjhgfsoika

--- models/daily_weather.sql
+++ models/daily_weather.sql
@@ -8,10 +8,24 @@ pressure,
 humidity,
 clouds
 from
-{{ source('demo', 'weather') }}  
-limit 10  
+{{ source('demo', 'weather') }}   
+),
+
+daily_weather_egg as(
+
+select
+daily_weather,
+weather,
+round(avg(temp),2) as avg_temp,
+round(avg(pressure),2) as avg_pressure,
+round(avg(humidity),2) as avg_humidity,
+round(avg(clouds),2) as avg_clouds
+
+from daily_weather
+group by daily_weather, weather
+Qualify ROW_NUMBER() OVER (PARTITION BY daily_weather ORDER BY count(weather) desc) = 1
 )
 
 Select
 * 
-from daily_weather
+from daily_weather_egg
